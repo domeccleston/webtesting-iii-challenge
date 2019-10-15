3f1b6288975580d3712820d1771e04f36ffe8940
Dom <domeccleston@gmail.com>
2019-10-15 13:25:35 +0100

    wip

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,3 +10,24 @@ test("Gate defaults to open unlocked state", () => {
   expect(display.getByText(/open/i)).toBeInTheDocument();
   expect(display.getByText(/unlocked/i)).toBeInTheDocument();
 });
+
+// 4. Display 'closed' if the closed prop is true and 'open' otherwise
+test("gate displays closed when rendered with closed prop", () => {
+    const closedGate = rtl.render(<Display closed={true} />);
+    expect(closedGate.getByText(/closed/i)).toBeInTheDocument();
+    const openGate = rtl.render(<Display closed={false} />);
+    expect(openGate.getByText(/open/i)).toBeInTheDocument();
+})
+
+// 5. Display 'locked' if the locked prop is true and 'unlocked' otherwise
+
+test("gate displays locked when rendered with locked prop", () => {
+    const lockedGate = rtl.render(<Display locked={true} />);
+    expect(lockedGate.getByText(/locked/i)).toBeInTheDocument();
+    const unlockedGate = rtl.render(<Display locked={false} />);
+    expect(unlockedGate.getByText(/unlocked/i)).toBeInTheDocument();
+})
+
+// 6. Alter classes when locked or unlocked to use red-led class rather than green-led:
+// not implementing this test as Gabe advises against testing class lists, and previous tests
+// already cover whether UI is updating to reflect changes in component state   

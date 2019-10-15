5fdbc16fac92d76f2a42069915c0cdf41fc95015
Dom <domeccleston@gmail.com>
2019-10-15 12:45:27 +0100

    gate defaults to open/unlocked

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+// 1. Gate defaults to unlocked and open
+
+test("Gate defaults to open unlocked state", () => {
+  const display = rtl.render(<Display />);
+  expect(display.getByText(/open/i)).toBeInTheDocument();
+  expect(display.getByText(/unlocked/i)).toBeInTheDocument();
+});
